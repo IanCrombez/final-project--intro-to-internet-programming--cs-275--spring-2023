d9a9134890094f316ac46425a05975ec3181ba73
IanCrombez <icrombez@crombezclan.com>
2023-05-07 23:21:10 -0400

    Fixed a part of the project i missed
    i changed my code to not be an input box but the popup wanted in the instructions

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,25 +1,51 @@
-let printMatrix = () => {
-    let size = parseInt(document.getElementById(`matrix-size`).value);
-    if (isNaN(size) || size <= 0) {
-        alert(`Please enter a positive integer.`);
-        return;
-    }
-
-    let matrix = [];
+let makeMatrix = (size, matrix) =>
+{
     for (let rowM = 0; rowM < size; rowM++) {
         matrix[rowM] = [];
         for (let colM = 0; colM < size; colM++) {
             matrix[rowM][colM] = rowM * size + colM + 1;
         }
     }
+    return matrix;
+};
+let swap = (rowM, colM, matrix, size) =>
+{
+    let temp = matrix[rowM][colM];
+    matrix[rowM][colM] = matrix[size - 1 - rowM][size - 1 - colM];
+    matrix[size - 1 - rowM][size - 1 - colM] = temp;
+};
 
+window.onload = () =>
+{
+    let size = window.prompt(`Enter the size of the matrix`);
+    //if size not a number or less then 2
+    while (isNaN(parseInt(size)) || parseInt(size) < 2)
+    {
+        if (isNaN(parseInt(size)))
+        {
+            alert(`Input needs to be a number`);
+        }
+        else
+        {
+            alert(`Input needs to be bigger then 1`);
+        }
+
+        size = window.prompt(`Enter the size of the matrix`);
+    }
+    let matrix = [];
+    makeMatrix(size, matrix);
+    // printing the table
     let table = document.createElement(`table`);
     table.id = `matrix-table`;
-    for (let rowM = 0; rowM < size; rowM++) {
+    for (let rowM = 0; rowM < size; rowM++)
+    {
         let row = document.createElement(`tr`);
-        for (let colM = 0; colM < size; colM++) {
+        for (let colM = 0; colM < size; colM++)
+        {
             let cell = document.createElement(`td`);
-            if (rowM + colM === size - 1) {
+            //color the diaganal yellow
+            if (rowM + colM === size - 1)
+            {
                 cell.style.backgroundColor = `yellow`;
             }
             cell.innerText = matrix[rowM][colM];
@@ -32,21 +58,24 @@ let printMatrix = () => {
     container.appendChild(table);
 
     // Reverse the matrix along the diagonal from bottom left to top right
-    for (let rowM = 0; rowM < size; rowM++) {
-        for (let colM = 0; colM < size-rowM; colM++) {
-            let temp = matrix[rowM][colM];
-            matrix[rowM][colM] = matrix[size - 1 - rowM][size - 1 - colM];
-            matrix[size - 1 - rowM][size - 1 - colM] = temp;
+    for (let rowM = 0; rowM < size; rowM++)
+    {
+        for (let colM = 0; colM < size-rowM; colM++)
+        {
+            swap(rowM, colM, matrix, size);
         }
     }
-
+    //print the reversed table
     let reversedTable = document.createElement(`table`);
     reversedTable.style.margin = `20px auto`;
-    for (let rowM = 0; rowM < size; rowM++) {
+    for (let rowM = 0; rowM < size; rowM++)
+    {
         let row = document.createElement(`tr`);
-        for (let colM = 0; colM < size; colM++) {
+        for (let colM = 0; colM < size; colM++)
+        {
             let cell = document.createElement(`td`);
-            if (rowM + colM === size - 1) {
+            if (rowM + colM === size - 1)
+            {
                 cell.style.backgroundColor = `yellow`;
             }
             cell.innerText = matrix[rowM][colM];
